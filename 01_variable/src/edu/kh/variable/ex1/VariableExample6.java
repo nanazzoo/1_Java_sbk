ab6cec307ce51fb2e78193c25419dde2a1b065e3
nanazzoo <101784680+nanazzoo@users.noreply.github.com>
2022-08-05 16:34:29 +0900

    수정

--- 01_variable/src/edu/kh/variable/ex1/VariableExample6.java
+++ 01_variable/src/edu/kh/variable/ex1/VariableExample6.java
@@ -3,7 +3,8 @@ package edu.kh.variable.ex1;
 public class VriableExample6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("새로운 클래스");
 
 	}
 
