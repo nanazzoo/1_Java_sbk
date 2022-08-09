976092f79cbdb9ef33afaefc41b4457cd5707121
nanazzoo <bboya0518@naver.com>
2022-08-09 13:54:23 +0900

    오전 수업

--- 03_control/src/edu/kh/control/condition/ConditionExample.java
+++ 03_control/src/edu/kh/control/condition/ConditionExample.java
@@ -89,9 +89,141 @@ public class ConditionExample {
 		}
 		
 		System.out.println("\n [if문 예시 2 종료] \n");
+		
+		
+		
+		
+		
 	}
 	
+//	if문 예시 3번
+		public void ex3() {
+			System.out.println("\n [if문 예시 3] \n");
+			/*
+			 *  if - else if - else 
+			 */
+			
+			
+			Scanner sc = new Scanner(System.in);
+			
+			
+			System.out.print("1~12 사이의 정수를 입력해주세요 : ");
+			int input = sc.nextInt();
+			
+			
+			String season; //변수 선언
+			
+			
+			if(input >=3 && input <= 5) {
+				season = "봄";
+			} else if (input >= 6 && input <= 8) {
+				season = "여름";
+			} else if (input >= 9 && input <= 11) {
+				season = "가을";
+			} else if (input == 12 || input == 1 || input ==2) {
+				season = "겨울";
+			} else {
+				season = "해당하는 계절이 없습니다.";
+			}
+			
+			
+			System.out.println(season);
+			
+			System.out.println("\n [if문 예시 3 종료] \n");
+		}
 	
+		
+//		실습 예제
+		public void ex4() {
+			
+			
+//			나이를 입력받아 13세 이하면 "어린이", 
+//			13세 초과, 19세 이하면 "청소년"
+//			19세 초과 시 성인 
+			
+			
+			Scanner sc = new Scanner(System.in);
+			
+			
+			System.out.print("나이를 입력해주세요 : ");
+			int input = sc.nextInt();
+			
+			
+			String result;
+			
+			
+			if (input <= 13) {
+				result = "어린이";
+				
+			} else if (input <= 19) {
+				result = "청소년";
+				
+			} else {
+				result = "성인";
+			}
+			
+			
+			System.out.println(result);
+			
+			
+		}
+		
+//	실습 예제
+		public void ex5() {
+			
+			// 놀이기구 탑승 제한 검사
+		      
+		      // 나이가 12세 이상, 키 140.0cm 이상 일 경우에만 "탑승 가능"
+		      // 나이가 12미만인 경우 : "적정 연령이 아닙니다."
+		      // 키가 140.0cm 미만 : "적정 키가 아닙니다."
+		      // 나이를 0세 미만, 100세 초과 시 : "잘못 입력 하셨습니다."
+			
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("나이 : ");
+			int age = sc.nextInt();
+			
+			System.out.print("키 : ");
+			double height = sc.nextInt();
+			
+			
+			String result;
+			
+			
+//			if (age < 0 || age > 100) {
+//				result = "잘못 입력 하셨습니다";
+//			} else {
+//				if(age < 12) {
+//					result = "적정 연령이 아닙니다.";
+//				} else {
+//					if (height < 140.0) {
+//						result = "적정 키가 아닙니다";
+//					}else {
+//						result = "탑승 가능";
+//					}
+//				}
+//			}
+//			
+//			
+			if (age < 0 || age > 100) {
+				result = "잘못 입력하셨습니다";
+				
+			} else if (age < 12) {
+				result = "적정 연령이 아닙니다";
+				
+			} else if (height < 140.0) {
+				result = "적정 키가 아닙니다";
+				
+			} else {
+				result = "탑승 가능";
+			}
+			
+						
+			System.out.println(result);
+						
+			
+		}
 	
 	
 	
--- 03_control/src/edu/kh/control/condition/ConditionRun.java
+++ 03_control/src/edu/kh/control/condition/ConditionRun.java
@@ -1,19 +1,41 @@
 package edu.kh.control.condition;
 
+import java.util.Scanner;
+
 //코드를 실행시키는 부분(시키는 사람)
 public class ConditionRun {
 
 	public static void main(String[] args) {
 		
 		ConditionExample ex = new ConditionExample();
-		
-	
+
 //		ex.test1();
 //		ex.test2();
 		
 //		ex.ex1();
-		ex.ex2();
+//		ex.ex2();
+//		ex.ex3();
+//		ex.ex4();
+//		ex.ex5();
+		
+		
+		
+		SwitchExample switchEx = new SwitchExample();
+		
+//		switchEx.ex1();
+//		switchEx.ex2();
+//		switchEx.ex3();
+//		switchEx.ex4();
+		switchEx.ex5();
+		
+		
+		
 		
 	}
+	
+	
+	
+	
+	
 	
 }
