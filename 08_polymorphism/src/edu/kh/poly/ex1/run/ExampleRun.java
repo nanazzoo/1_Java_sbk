c1050ad3caee88ac7a804c5dd445e7322a9b2694
nanazzoo <>
2022-08-23 13:59:50 +0900

    0823 오전수업

---
+++ 08_polymorphism/src/edu/kh/poly/ex1/run/ExampleRun.java
@@ -0,0 +1,15 @@
+package edu.kh.poly.ex1.run;
+
+import edu.kh.poly.ex1.model.service.Example1;
+
+public class ExampleRun {
+
+	public static void main(String[] args) {
+		
+		Example1 exam1 = new Example1();
+		
+		
+		
+	}
+
+}
