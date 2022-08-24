ef8126ed3b7e0803f1a3ba54c3435028aed82cfb
nanazzoo <>
2022-08-24 17:46:37 +0900

    Update Main.java

--- practice/src/Main.java
+++ practice/src/Main.java
@@ -5,22 +5,17 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-
-		int score = sc.nextInt();
-	
-		if(score>=90 && score <=100) {
-			System.out.println("A");
-		} else if(score>=80 && score <=89) {
-			System.out.println("B");
-		} else if(score>=70 && score <=79){
-			System.out.println("C");
-		} else if(score>=60 && score <=69) {
-			System.out.println("D");
+		int H = sc.nextInt();
+		int M = sc.nextInt();
+		
+		if(M>45) {
+			System.out.print(H + " ");
+			System.out.print((M-45));
 		} else {
-			System.out.println("F");
-			
+			System.out.println((H-1)+ ":" + (60-(45-M)));
 		}
 		
+		sc.close();
 		
 	}
 
